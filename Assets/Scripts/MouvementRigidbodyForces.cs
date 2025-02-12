4071376ca392189cdb42e634e217dedb9a40d1ce
GabbDup <gabriel.dupras01@gmail.com>
2025-02-12 15:52:36 -0500

    modification rigidbody

--- Assets/Scripts/MouvementRigidbodyForces.cs
+++ Assets/Scripts/MouvementRigidbodyForces.cs
@@ -6,12 +6,15 @@ public class MouvementRigidbodyForces : MonoBehaviour
 {
     public Vector3 forceInitiale = Vector3.zero;
     private Rigidbody _rb;
+    //private AudioSource _as;
 
     void Start()
     {
         // Prends une réference au Rigidbody attaché a cet objet.
         _rb = GetComponent<Rigidbody>();
+       // _as = GetComponent<AudioSource>();
 
         _rb.AddForce(forceInitiale);
     }
+
 }
